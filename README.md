a46e588970e1e9420c1474afe92a9ebfb99ceb58
alxmtzr <ametzler188@googlemail.com>
2024-08-10 16:33:09 +0200

    updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # GetYourLocalTime
-A small cross-platform app to display the local time by given location. First attempt to build a cross-platform app developed with kotlin compose multiplatform.
 
+This is a simple Kotlin Compose Multiplatform application that allows users to select a country and display its local time.
+
+## Overview
+
+This project is built using [Kotlin Multiplatform](https://kotlinlang.org/docs/multiplatform.html) and [Jetpack Compose](https://developer.android.com/jetpack/compose), 
+enabling the app to run on multiple platforms (Android, iOS Desktop) with a single codebase. The app features an interface where users can choose a country from a list and 
+view the current local time for that selected country.
+
+## Features
+
+- Select a country from a list.
+- Display the local time of the selected country.
+- Cross-platform support using Kotlin Multiplatform.
+
+This project is from the Tutorial [Create your own application](https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-multiplatform-new-project.html) from jetbrains.com.
--- composeApp/src/commonMain/kotlin/de/alxmtzr/getyourlocaltime/App.kt
+++ composeApp/src/commonMain/kotlin/de/alxmtzr/getyourlocaltime/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -24,15 +25,40 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import getyourlocaltime.composeapp.generated.resources.Res
 import getyourlocaltime.composeapp.generated.resources.compose_multiplatform
+import getyourlocaltime.composeapp.generated.resources.eg
+import getyourlocaltime.composeapp.generated.resources.fr
+import getyourlocaltime.composeapp.generated.resources.id
+import getyourlocaltime.composeapp.generated.resources.jp
+import getyourlocaltime.composeapp.generated.resources.mx
 import kotlinx.datetime.Clock
 import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.compose.resources.DrawableResource
+
+data class Country(val name: String, val zone: TimeZone, val image: DrawableResource)
+
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(zone).time
+
+    return "The time in $location is ${localTime.formatted()}"
+}
+
+val defaultCountries = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo"), Res.drawable.jp),
+    Country("France", TimeZone.of("Europe/Paris"), Res.drawable.fr),
+    Country("Mexico", TimeZone.of("America/Mexico_City"), Res.drawable.mx),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta"), Res.drawable.id),
+    Country("Egypt", TimeZone.of("Africa/Cairo"), Res.drawable.eg)
+)
 
 @Composable
 @Preview
-fun App() {
+fun App(countries: List<Country> = defaultCountries) {
     MaterialTheme {
         var showCountries by remember { mutableStateOf(false) }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
@@ -49,76 +75,33 @@ fun App() {
                     expanded = showCountries,
                     onDismissRequest = { showCountries = false }
                 ) {
-                    countries().forEach { (name, zone) ->
-                        DropdownMenuItem(onClick = {
-                            timeAtLocation = currentTimeAt(zone.id)?: "Invalid Location"
-                            showCountries = false
-                        }) {
-                            Text(text = name)
+                    countries.forEach { (name, zone, image) ->
+                        DropdownMenuItem(
+                            onClick = {
+                                timeAtLocation = currentTimeAt(name, zone)
+                                showCountries = false
+                            }
+                        ) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Image(
+                                    painterResource(image),
+                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
+                                    contentDescription = "$name flag"
+                                )
+                                Text(name)
+                            }
                         }
                     }
                 }
             }
-            Button(
-                onClick = { showCountries = !showCountries },
-                modifier = Modifier.padding(top = 10.dp)
-            ) {
+
+            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                onClick = { showCountries = !showCountries }) {
                 Text("Select Location")
             }
         }
     }
 }
-
-data class Country(val name: String, val zone: TimeZone)
-
-fun currentTimeAt(location: String): String? {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
-
-    return try {
-        val time = Clock.System.now()
-        val zone = TimeZone.of(location)
-        val localTime = time.toLocalDateTime(zone).time
-        "The time in $location is ${localTime.formatted()}"
-    }catch (ex: IllegalTimeZoneException) {
-        null
-    }
-}
-
-fun countries() = listOf(
-    Country("Japan", TimeZone.of("Asia/Tokyo")),
-    Country("France", TimeZone.of("Europe/Paris")),
-    Country("Germany", TimeZone.of("Europe/Berlin")),
-    Country("United States", TimeZone.of("America/New_York")),
-    Country("India", TimeZone.of("Asia/Kolkata")),
-    Country("Russia", TimeZone.of("Europe/Moscow")),
-    Country("United Kingdom", TimeZone.of("Europe/London")),
-    Country("Canada", TimeZone.of("America/Toronto")),
-    Country("Australia", TimeZone.of("Australia/Sydney")),
-    Country("China", TimeZone.of("Asia/Shanghai")),
-    Country("Brazil", TimeZone.of("America/Sao_Paulo")),
-    Country("Argentina", TimeZone.of("America/Buenos_Aires")),
-    Country("Mexico", TimeZone.of("America/Mexico_City")),
-    Country("South Africa", TimeZone.of("Africa/Johannesburg")),
-    Country("Egypt", TimeZone.of("Africa/Cairo")),
-    Country("South Korea", TimeZone.of("Asia/Seoul")),
-    Country("Turkey", TimeZone.of("Europe/Istanbul")),
-    Country("Italy", TimeZone.of("Europe/Rome")),
-    Country("Spain", TimeZone.of("Europe/Madrid")),
-    Country("Netherlands", TimeZone.of("Europe/Amsterdam")),
-    Country("Sweden", TimeZone.of("Europe/Stockholm")),
-    Country("Switzerland", TimeZone.of("Europe/Zurich")),
-    Country("Austria", TimeZone.of("Europe/Vienna")),
-    Country("Belgium", TimeZone.of("Europe/Brussels")),
-    Country("Ireland", TimeZone.of("Europe/Dublin")),
-    Country("Portugal", TimeZone.of("Europe/Lisbon")),
-    Country("Denmark", TimeZone.of("Europe/Copenhagen")),
-    Country("Norway", TimeZone.of("Europe/Oslo")),
-    Country("Finland", TimeZone.of("Europe/Helsinki")),
-    Country("Greece", TimeZone.of("Europe/Athens")),
-    Country("Iceland", TimeZone.of("Atlantic/Reykjavik")),
-    Country("Greenland", TimeZone.of("America/Godthab")),
-    Country("United Arab Emirates", TimeZone.of("Asia/Dubai")),
-).sortedBy { it.name }
 
 
 
